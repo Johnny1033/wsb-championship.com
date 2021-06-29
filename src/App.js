552f19ff919905786c62519926bbdfc75592706d
John M <Johnny1033@gmail.com>
2021-06-29 17:02:09 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Hello.
+        </p>
+        <a
+          className="App-link"
+          href="https://www.linkedin.com/in/johnamessinger/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          This is not a phishing link
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
