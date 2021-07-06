a9ed22a2246c5400c8ad081c389872dd7f4ffd9a
John M <Johnny1033@gmail.com>
2021-07-06 14:49:59 -0400

    changed border color, winner text, and layout

--- src/index.js
+++ src/index.js
@@ -99,7 +99,7 @@ class Game extends React.Component {
     })
     let status;
     if (winner) {
-      status = 'Congratulations ' + winner;
+      status = 'Winner: ' + winner;
     } else {
       status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
     }
