b57af38bf46e1f91a9d125027d67bf97e0c25961
John M <44906628+Johnny1033@users.noreply.github.com>
2023-04-27 22:44:55 -0400

    gamecontainer to index.js

--- src/index.js
+++ src/index.js
@@ -138,6 +138,13 @@ class Game extends React.Component {
     return (
       <div className="game">
         <div className="game-container">
+          <div className="game-info">
+            <div>{status}</div>
+            <button onClick={() => this.toggleHistoryOrder()}>
+              Reverse history
+            </button>
+            <ol>{this.state.reverseHistory ? moves.reverse() : moves}</ol>
+          </div>
           <div className="game-board">
             <Board
               squares={current.squares}
@@ -147,12 +154,10 @@ class Game extends React.Component {
           </div>
           <Leaderboard scores={this.state.scores} />
         </div>
-        <div className="game-info">
-          <div>{status}</div>
-          <button onClick={() => this.toggleHistoryOrder()}>
-            Reverse history
-          </button>
-          <ol>{this.state.reverseHistory ? moves.reverse() : moves}</ol>
+        <div className="footer">
+          <div className="links">
+            ...
+          </div>
         </div>
       </div>
     );
